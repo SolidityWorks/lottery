6c1a5a5e6c845059442301b7a0c9662017ac783f
Darius <darius99dark@mail.ru>
2022-04-14 23:28:11 +0500

    refactor: added loader on main screen, side notification, ticket cost

--- src/contracts/accessors.js
+++ src/contracts/accessors.js
@@ -19,14 +19,19 @@ export const writeContract = async (func, args = []) => {
     console.log(e);
   }
 }
-export const writePayableContract = async (func, val, args = []) => {
+export const writePayableContract = async (func, val, args = [], setLoader, setSuccess) => {
   try {
-    const result = await func(...args, {value: val});
+    const result = await func(...args, { value: val });
     console.log('Writing...');
+    setLoader(true);
     await result.wait() // wait is only for write actions
+    setLoader(false);
+    setSuccess('success');
     console.log(result);
     return result;
   } catch (e) {
+    setLoader(false);
+    setSuccess('failed');
     console.log(e);
   }
 }
